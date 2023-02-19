265c554142a5e8a23a73452a1a6df3f123e783e1
Karthik <karthik5112001@gmail.com>
2023-02-19 10:59:02 +0530

    commit

---
+++ 7-Arrays/Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays{
+    public static void main(String[] args){
+
+
+        //--------------primitive data type---------------
+        int a = 60; 
+
+        //-------------Non-primitive data type------------
+
+        int numbers[] = new int[]{10,20,30,40,50};  // length : 5
+
+        for(int i=0; i<5; i++){  // scope or block
+             System.out.println( numbers[i]+2 ); // 10+2=12 , 20+2
+        }
+                           
+       
+
+
+    }
+}
